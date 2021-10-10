18148db41bf954b94d2c35c8c3575da87faee18f
avikgenxt <avik.genxt@gmail.com>
2021-10-10 00:16:01 -0400

    new code

--- lambda/src/lambda/basic/function/JavaFunctional3.java
+++ lambda/src/lambda/basic/function/JavaFunctional3.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class JavaFunctional3 {
 
 	public static void main(String args[]) {
+		System.out.println("************************REDUCE AND SUM**********************************");
 		Integer i =  listPerson().stream()
 		.map((Person::getAge))
 		.map(Integer::valueOf)
@@ -15,16 +16,21 @@ public class JavaFunctional3 {
 		
 		System.out.println(i);
 		System.out.println("**********************************************************");
-				
+		System.out.println("**********************COLLECTORS TO LIST************************************");
+		//Get list of names where age is greater than 30		
 		List<String> ss =  listPerson().stream()
 		.filter(e->Integer.valueOf(e.getAge())>30)
 		.map(Person::getName)
 		.map(String::toUpperCase)
 		.collect(toList());
-		/*.reduce(
-				new ArrayList<String>(),
-				(names,name)->{names.add(name); return names;},
-				(names1, names2)->{
+		/* toList () ====
+		 * start with an empty list implementation 1
+		 * create a list of objects and add one element 2
+		 * Add the lists created in step 2
+		 *.reduce(
+				new ArrayList<String>(), //Step 1
+				(names,name)->{names.add(name); return names;}, //Step2
+				(names1, names2)->{ //step3
 					names1.addAll(names2);
 					return names1;
 				} 
@@ -32,19 +38,21 @@ public class JavaFunctional3 {
 		
 		System.out.println(ss);
 		System.out.println("**********************************************************");
+		System.out.println("*********************COLLECTORS TO MAP*************************************");
 		
+		//Create a map with name as key and age as value
 		System.out.println(
 				listPerson()
 				.stream().
 				collect(toMap(Person::getName, Person::getAge)));
-		
 		System.out.println("**********************************************************");
+		System.out.println("***********************PARTITION BY***********************************");
 		System.out.println(
 				listPerson()
 				.stream()
 				.collect(partitioningBy(person->Integer.valueOf(person.getAge())%2==0)));
-		
 		System.out.println("**********************************************************");
+		System.out.println("********************GROUPING BY MAPPING**************************************");
 		System.out.println(
 				listPerson()
 				.stream()
@@ -56,11 +64,21 @@ public class JavaFunctional3 {
 		//means
 		//Collectors(function, collectors(function, collectors))
 		System.out.println("**********************************************************");
+		System.out.println("**************************GROUPING BY ********************************");
 		System.out.println(
 				listPerson2()
 				.stream()
 				.collect(
 						groupingBy(person->person.getName(), counting()))
+				);
+
+		System.out.println("**********************************************************");
+		System.out.println("*************************GROUP BY COLLECTING AND THEN*********************************");
+		System.out.println(
+				listPerson2()
+				.stream()
+				.collect(
+						groupingBy(person->person.getName(),collectingAndThen(counting(), Long::intValue)))
 				);
 
 		
